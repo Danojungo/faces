a580412fc6f55a463846e70b2d01f3b385f3e321
dano <dano@jungo.com>
2022-11-28 16:46:06 +0200

    this is a correction requirements.txt

--- training.py
+++ training.py
@@ -9,7 +9,7 @@ import helper_functions as hf
 path_to_images = "/home/dano/clearml_poc/face_validator_data"
 image_dims = [50, 50, 1]
 
-task = Task.init(project_name="face_validatior", task_name="first task")
+task = Task.init(project_name="face_validatior", task_name="second task")
 params_dictionary = {'epochs': 10, 'lr': 0.0005, 'patience': 5}
 task.connect(params_dictionary)
 
@@ -18,7 +18,6 @@ def main():
     this file creates and trains a model, then after analysing the plots enter 'y' to save or anything else to not save.
     :return:
     """
-    print(os.getcwd())
     data = dataset.Dataset(path_to_images, image_dims, labels_mode='categorical', seed=24, batch_size=16)
     cnn = model.My_Cnn(image_dims, logits=True, loss_function='categorical', loaded_model=0, debug_printing=0)
     cnn.train_model(data.train_set, data.val_set, params_dictionary['epochs'],
